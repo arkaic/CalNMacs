552280077954e86200432b07714afea723ab0a79
Henry Lin <arkhenry@gmail.com>
2016-02-20 16:35:36 -0500

    feat: added stub for add button in food list activity refactor: renamed a few variables

--- app/src/main/java/org/arkaic/calnmacs/FoodListActivity.java
+++ app/src/main/java/org/arkaic/calnmacs/FoodListActivity.java
@@ -33,11 +33,11 @@ public class FoodListActivity extends AppCompatActivity {
         FoodDbHelper dbHelper = new FoodDbHelper(getApplicationContext());
         _db = dbHelper.getWritableDatabase();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton refresh = (FloatingActionButton) findViewById(R.id.refreshButton);
+        refresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextView t = (TextView)findViewById(R.id.txt);
+                TextView t = (TextView) findViewById(R.id.txt);
                 t.setText("FOOD DB\n");
                 t.append(FoodDbColumns.COLUMN_NAME_COL1 + " ");
                 t.append(FoodDbColumns.COLUMN_NAME_COL2 + " ");
@@ -48,29 +48,35 @@ public class FoodListActivity extends AppCompatActivity {
                 t.append(FoodDbColumns.COLUMN_NAME_COL7 + "\n");
                 Cursor c = _db.rawQuery("SELECT * from foods;", null);
                 if (c.moveToFirst()) {
-                   while (!c.isAfterLast()) {
-                       String name = c.getString(c.getColumnIndex(FoodDbContract.FoodDbColumns.COLUMN_NAME_COL1));
-                       t.append(c.getString(c.getColumnIndex(FoodDbColumns.COLUMN_NAME_COL1)) + " | ");
-                       t.append(c.getString(c.getColumnIndex(FoodDbColumns.COLUMN_NAME_COL2)) + " | ");
-                       t.append(c.getString(c.getColumnIndex(FoodDbColumns.COLUMN_NAME_COL3)) + " | ");
-                       t.append(c.getString(c.getColumnIndex(FoodDbColumns.COLUMN_NAME_COL4)) + " | ");
-                       t.append(c.getString(c.getColumnIndex(FoodDbColumns.COLUMN_NAME_COL5)) + " | ");
-                       t.append(c.getString(c.getColumnIndex(FoodDbColumns.COLUMN_NAME_COL6)) + " | ");
-                       t.append(c.getString(c.getColumnIndex(FoodDbColumns.COLUMN_NAME_COL7)) + "\n");
-                       c.moveToNext();
-                   }
+                    while (!c.isAfterLast()) {
+                        String name = c.getString(c.getColumnIndex(FoodDbContract.FoodDbColumns.COLUMN_NAME_COL1));
+                        t.append(c.getString(c.getColumnIndex(FoodDbColumns.COLUMN_NAME_COL1)) + " | ");
+                        t.append(c.getString(c.getColumnIndex(FoodDbColumns.COLUMN_NAME_COL2)) + " | ");
+                        t.append(c.getString(c.getColumnIndex(FoodDbColumns.COLUMN_NAME_COL3)) + " | ");
+                        t.append(c.getString(c.getColumnIndex(FoodDbColumns.COLUMN_NAME_COL4)) + " | ");
+                        t.append(c.getString(c.getColumnIndex(FoodDbColumns.COLUMN_NAME_COL5)) + " | ");
+                        t.append(c.getString(c.getColumnIndex(FoodDbColumns.COLUMN_NAME_COL6)) + " | ");
+                        t.append(c.getString(c.getColumnIndex(FoodDbColumns.COLUMN_NAME_COL7)) + "\n");
+                        c.moveToNext();
+                    }
                 }
                 c.close();
-                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                 //       .setAction("Action", null).show();
             }
         });
 
-        FloatingActionButton closeb = (FloatingActionButton) findViewById(R.id.closebutton);
+        FloatingActionButton closeb = (FloatingActionButton) findViewById(R.id.closeButton);
         closeb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        FloatingActionButton addButton = (FloatingActionButton) findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO open a dialogue for adding custom food
             }
         });
     }
