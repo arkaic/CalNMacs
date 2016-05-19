312c83007226a9b0dee4850779e13a21def08e96
Henry Lin <arkhenry@gmail.com>
2016-05-19 18:12:26 -0400

    refactor: Food's proteintoratio get method will return number as a percentage, rather than a decimal

--- app/src/main/java/org/arkaic/calnmacs/Food.java
+++ app/src/main/java/org/arkaic/calnmacs/Food.java
@@ -71,7 +71,7 @@ public class Food implements Serializable {
     }
     public String name() {return mName; }
     public String unit() {return mUnit; }
-    public double proteinToCalRatio() {return mProteinRatio; }
+    public double proteinToCalRatio() {return Math.floor(mProteinRatio * 1e4) / 1e4 * 100; }
 
     @Override
     public String toString() {
