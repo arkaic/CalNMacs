b05da2cc204ea647be5551abb8c3146762307a1c
Henry Lin <arkhenry@gmail.com>
2016-05-21 18:30:18 -0400

    feat: Trying to add a food to the db without filling every field will do nothing.

--- app/src/main/java/org/arkaic/calnmacs/FoodDbFragment.java
+++ app/src/main/java/org/arkaic/calnmacs/FoodDbFragment.java
@@ -169,51 +169,66 @@ public class FoodDbFragment extends ListFragment {
                 // open a dialogue for adding custom food
                 AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
                 final View diaView = getLayoutInflater(savedInstanceState).inflate(R.layout.dialog_add_fooddb, null);
-                builder.setView(diaView);
-                builder.setTitle("Add a new food to the food list");
+                final AlertDialog dia = builder.setView(diaView)
+                        .setTitle("Add a new food to the food list")
+                        .setPositiveButton(android.R.string.ok, null)
+                        .setNegativeButton(android.R.string.cancel, null)
+                        .create();
+
+                dia.setOnShowListener(new DialogInterface.OnShowListener() {
+                    @Override
+                    public void onShow(DialogInterface dialog) {
+                        dia.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                // Check if any text fields are empty
+                                String name = ((EditText)diaView.findViewById(R.id.name_add)).getText().toString();
+                                String unit = ((EditText)diaView.findViewById(R.id.unit_type_add)).getText().toString();
+                                String carbStr = ((EditText)diaView.findViewById(R.id.carb_add)).getText().toString();
+                                String fatStr = ((EditText)diaView.findViewById(R.id.fat_add)).getText().toString();
+                                String proteinStr = ((EditText)diaView.findViewById(R.id.protein_add)).getText().toString();
+                                String calsStr = ((EditText)diaView.findViewById(R.id.calorie_add)).getText().toString();
+
+                                if (name.trim().length() == 0 || unit.trim().length() == 0 ||
+                                        carbStr.trim().length() == 0 || fatStr.trim().length() == 0 ||
+                                        proteinStr.trim().length() == 0 || calsStr.trim().length() == 0) {
+                                    // TODO popup dialog warning but do not leave alertdialog
+                                } else {
+                                    ContentValues cv = new ContentValues();
+                                    cv.put(FoodDbColumns.FOOD_NAME_COLUMN, name);
+                                    cv.put(FoodDbColumns.UNIT_COLUMN, unit);
+                                    cv.put(FoodDbColumns.CARBS_COLUMN, Double.parseDouble(carbStr));
+                                    cv.put(FoodDbColumns.FAT_COLUMN, Double.parseDouble(fatStr));
+                                    cv.put(FoodDbColumns.PROTEIN_COLUMN, Double.parseDouble(proteinStr));
+                                    cv.put(FoodDbColumns.CALS_COLUMN, Double.parseDouble(calsStr));
+                                    cv.put(FoodDbColumns.RATIO_COLUMN,
+                                            Double.parseDouble(proteinStr) * 4 / Double.parseDouble(calsStr));
+                                    mDb.insert(FoodDbColumns.TABLE_NAME, null, cv);
+                                    mAdapter.changeCursor(mDb.rawQuery("SELECT * FROM foods ORDER BY " +
+                                            FoodDbColumns.FOOD_NAME_COLUMN + " COLLATE NOCASE;", null));
+                                    mAdapter.notifyDataSetChanged();
+                                    dia.dismiss();
+                                }
+                            }
+                        });
+                    }
+                });
 
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        // Check if any text fields are empty
-                        String name = ((EditText)diaView.findViewById(R.id.name_add)).getText().toString();
-                        String unit = ((EditText)diaView.findViewById(R.id.unit_type_add)).getText().toString();
-                        String carbStr = ((EditText)diaView.findViewById(R.id.carb_add)).getText().toString();
-                        String fatStr = ((EditText)diaView.findViewById(R.id.fat_add)).getText().toString();
-                        String proteinStr = ((EditText)diaView.findViewById(R.id.protein_add)).getText().toString();
-                        String calsStr = ((EditText)diaView.findViewById(R.id.calorie_add)).getText().toString();
-
-                        if (name.trim().length() == 0 || unit.trim().length() == 0 ||
-                                carbStr.trim().length() == 0 || fatStr.trim().length() == 0 ||
-                                proteinStr.trim().length() == 0 || calsStr.trim().length() == 0) {
-                            // popup dialog warning but do not leave alertdialog
-                        } else {
-                            ContentValues cv = new ContentValues();
-                            cv.put(FoodDbColumns.FOOD_NAME_COLUMN, name);
-                            cv.put(FoodDbColumns.UNIT_COLUMN, unit);
-                            cv.put(FoodDbColumns.CARBS_COLUMN, Double.parseDouble(carbStr));
-                            cv.put(FoodDbColumns.FAT_COLUMN, Double.parseDouble(fatStr));
-                            cv.put(FoodDbColumns.PROTEIN_COLUMN, Double.parseDouble(proteinStr));
-                            cv.put(FoodDbColumns.CALS_COLUMN, Double.parseDouble(calsStr));
-                            cv.put(FoodDbColumns.RATIO_COLUMN,
-                                    Double.parseDouble(proteinStr) * 4 / Double.parseDouble(calsStr));
-                            mDb.insert(FoodDbColumns.TABLE_NAME, null, cv);
-                            mAdapter.changeCursor(mDb.rawQuery("SELECT * FROM foods ORDER BY " +
-                                    FoodDbColumns.FOOD_NAME_COLUMN + " COLLATE NOCASE;", null));
-                            mAdapter.notifyDataSetChanged();
-                        }
 
                     }
                 });
 
-                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+//                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//
+//                    }
+//                });
 
-                    }
-                });
-
-                builder.create().show();
+                dia.show();
             }
         });
     }
